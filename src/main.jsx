d8ed0a4d5aadda675d92daedd0a91f52c035628d
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2026-08-21 18:28:27 +0700

    feat: add persistence on redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./Router.jsx";
@@ -9,18 +10,20 @@ import App from "./Router.jsx";
 import ThemeProvider from "./contexts/theme/ThemeProvider.jsx";
 import CartProvider from "./contexts/cart/CartProvider.jsx";
 
-import reduxStore from "./redux/store.js";
+import reduxStore, { persistor } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={reduxStore}>
-      <ThemeProvider>
-        <CartProvider>
-          <Router>
-            <App />
-          </Router>
-        </CartProvider>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <CartProvider>
+            <Router>
+              <App />
+            </Router>
+          </CartProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistCombineReducers,
+  persistReducer,
+} from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./slices/cartSlices.js";
 import productReducer from "./slices/productSlices.js";
 
-const store = configureStore({
-  reducer: {
-    cartState: cartReducer,
-    productState: productReducer,
+const storage = {
+  getItem: (key) => {
+    return Promise.resolve(window.localStorage.getItem(key));
   },
+  setItem: (key, value) => {
+    return Promise.resolve(window.localStorage.setItem(key, value));
+  },
+  removeItem: (key) => {
+    return Promise.resolve(window.localStorage.removeItem(key));
+  },
+};
+
+const persistCartConfig = {
+  key: "cart_product",
+  storage,
+  whitelist: ["cartState"],
+};
+const persistProductConfig = {
+  key: "product",
+  storage,
+  whitelist: ["products"],
+};
+
+const store = configureStore({
+  reducer: persistCombineReducers(persistCartConfig, {
+    cartState: cartReducer,
+    productState: persistReducer(persistProductConfig, productReducer),
+  }),
   devTools: import.meta.env.VITE_ENVIRONMENT === "development",
 });
+
+export const persistor = persistStore(store);
 
 export default store;
